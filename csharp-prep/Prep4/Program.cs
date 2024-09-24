17047251f674805c6c5ad13e483138a6d030933f
willatbyui <lag20001@byui.edu>
2024-09-25 01:15:46 +1200

    completed requirement1 of prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Will\'s Prep4 assignment. ");
+        List<int> UserList = UserInput();
+        ReturnList(UserList);
     }
+
+    static List<int> UserInput()
+    {
+        List<int> UserList = new List<int>();
+        int input;
+        
+        int input;
+        do
+        {
+            Console.WriteLine("Enter Numbers here: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                UserList.Add(input);
+            }
+        } while (input != 0)
+        {
+            return UserList;
+        }
+    }
+    static void ReturnList(List<int> numbers)
+    {
+        Console.WriteLine( "Your list contains the following numbers: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+
 }
