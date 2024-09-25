fe941ffa4f8d0f60933e60313dd5147beaa7371e
willatbyui <lag20001@byui.edu>
2024-09-25 12:02:24 +1200

    completed prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Will\'s Prep5 assignment");
+
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int favNum = PromptUserNum();
+        int favSquared = SquareNum(favNum);
+        DisplayResult(userName, favSquared);
+
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program. ");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNum()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNum(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string userName, int favSquared)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {favSquared}. ");
+    }
+
 }
