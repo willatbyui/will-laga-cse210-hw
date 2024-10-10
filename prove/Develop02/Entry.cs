c79d19a6779fc623ab38dd002f340fd62511b225
willatbyui <lag20001@byui.edu>
2024-10-11 12:36:42 +1300

    entries.cs

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -7,8 +7,56 @@ class Entry
 
 
 
+    public string Prompt {get;}
+    public string Response {get;}
+    public string Date {get;}
 
+    public Entry(string prompt, string response, string date)
+    {
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
 
+    public override string ToString()
+    {
+        return $"{Date} | {Prompt} | {Response}";
+    }
+
+    public static void SaveToFile(List<Entry> entries, string filename)
+    {
+        using(StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (var entry in entries)
+            {
+                writer.WriteLine(entry);
+            }
+        }
+        Console.WriteLine("journal saved. ");
+    }
+
+    public static List<Entry> LoadFromFile(string filename)
+    {
+        List<Entry> entries = new List<Entry>();
+
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine("Uh oh, file not found.");
+            return entries;
+        }
+
+        string[] lines = File.ReadAllLines(filename);
+        foreach (var line in lines)
+        {
+            var parts = line.Split('|');
+            if (parts.Length == 3)
+            {
+                entries.Add(new Entry(parts[0].Trim(), parts[1].Trim(), parts[2].Trim()));
+            }
+        }
+        Console.WriteLine("Entry Loaded successfully. ");
+        return entries;
+    }
 
 
 
