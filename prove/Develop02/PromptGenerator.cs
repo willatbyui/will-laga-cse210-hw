e61c0f895df265b831596883d79083f9b0d3cf6c
willatbyui <lag20001@byui.edu>
2024-10-11 12:46:45 +1300

    promptgenerator method

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -4,6 +4,21 @@ using System.Collections.Generic;
 class PromptGenerator
 {
 
+    private List<string> prompts = new List<string>
+    {
+        "Who was the most interesting person you met today?",
+        "What was the best part of your day?",
+        "What was the strongest emotion you felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "How did I see the Lord\'s hand in my life today?"
+    };
+
+    public string GetRandomPrompt()
+    {
+        Random random = new Random();
+        return prompts[random.Next(prompts.Count)];
+        
+    }
 
 
 
