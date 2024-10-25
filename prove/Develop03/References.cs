72138280e370efc4a0ad4d424c76734ec62acb70
willatbyui <lag20001@byui.edu>
2024-10-26 12:43:20 +1300

    restarting again

--- prove/Develop03/References.cs
+++ prove/Develop03/References.cs
@@ -3,20 +3,6 @@ using System.Collections.Generic;
 
 public class Reference
 {
-    public static string GetReference(int selection)
-    {
-        switch (selection)
-        {
-            case 1:
-                return "Proverbs 3: 5-6 ";
-
-            case 2:
-                return "Philippians 4:13";
-            case 3:
-                return
-        }
-    }
-
 
 
 
