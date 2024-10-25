845c34b21c5110b390386947dcb7d3fb57444687
willatbyui <lag20001@byui.edu>
2024-10-26 12:57:29 +1300

    refactor program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,2 +1,39 @@
 using System;
 using System.Collections.Generic;
+
+
+class Program
+{
+        static void Main(string[] args)
+        {
+        /* Initialize scripture object */
+                Scriptures scripture = new Scriptures();
+
+                Console.WriteLine(" Welcome to Will\'s scripture memorizing program. ");
+                Console.WriteLine("Press the enter key to start or type 'quit' to exit the program. ");
+
+                while (true)
+                {
+                        string input = Console.ReadLine();
+                        
+                        if (input == "quit")
+                        {
+                                break;
+                        }
+                        if (input == "")
+                        {
+                        /* Hide random word then display the scripture */
+                                scripture.HideRandom();
+                                scripture.Display();
+
+                        /* check if all words are hidden then end program */
+                                if (scripture.AllHidden())
+                                {
+                                        Console.WriteLine(" All words are hidden, nice job. The Program will now end... ");
+                                        break;
+                                }
+                        }
+                }
+
+        }
+}
