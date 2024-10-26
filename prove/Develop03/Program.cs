20820458251db0ac013ba7781ca4a6b183a8bd87
willatbyui <lag20001@byui.edu>
2024-10-26 14:58:02 +1300

    minor changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,18 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Initialize the ScripturesRepository
         ScriptureRepo repository = new ScriptureRepo();
         
-        // Get a random scripture from the repository
         Random random = new Random();
         List<string> keys = new List<string>(repository.GetAllKeys());
         string randomKey = keys[random.Next(keys.Count)];
 
-        // Retrieve the reference and text for the random scripture
         var (reference, text) = repository.GetScripture(randomKey);
 
-        // Initialize Scriptures object with the random scripture
         Scriptures scripture = new Scriptures(reference, text);
 
         Console.WriteLine(" Welcome to Will\'s scripture memorizing program. ");
