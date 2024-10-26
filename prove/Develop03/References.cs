be73ef63003bf5372579c796fbf0e4598ef34a6e
willatbyui <lag20001@byui.edu>
2024-10-26 13:07:20 +1300

    redo program.cs and start on reference.cs with mentor advice

--- prove/Develop03/References.cs
+++ prove/Develop03/References.cs
@@ -1,13 +1,34 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 
 public class Reference
 {
 
+/* private variables */
+    private string book;
+    private string chapter;
+    private string verse;
 
+/* Constructor for single verse */
+    public Reference( string book, string chapter, string verse)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.verse = verse;
+    }
+/*Constructor for multiple verse scriptures */
 
+    public Reference( string book, string chapter, string beginVerse, string endVerse)
+    {
+        this.book = book;
+        this.chapter = chapter;
+        this.verse = $"{beginVerse}-{endVerse}";
+    }
 
-
-
+    public string GetRef()
+    {
+        return $"{book} {chapter}: {verse} ";
+    }
 
 }
--- prove/Develop03/Scriptures.cs
+++ prove/Develop03/Scriptures.cs
@@ -4,5 +4,4 @@ using System.Collections.Generic;
 public class Scriptures
 {
 
-
 }
